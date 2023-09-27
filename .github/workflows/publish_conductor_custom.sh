287de7418940904f68e92cabce4026a6056a511d
sarmuru2 <sarmuru2@cisco.com>
2023-09-27 15:58:01 +0530

    ISICO-15112: report plugin is removed

--- .github/workflows/publish_conductor_custom.sh
+++ .github/workflows/publish_conductor_custom.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/CiscoM31/conductor-custom
 cd conductor-custom
-git checkout ISICO-15112-1
+git checkout main
 ./gradlew build publishToMavenLocal -x test
 cd ..
 rm -rf conductor-custom
